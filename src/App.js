988aca785365ec8b4b62d086ac6103e08f3a6e6b
Chandra Mouli <chandramouli@vyaparapp.in>
2021-06-13 18:24:04 +0530

    updated app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from "./pages/Home"
 
 function App() {
   return (
     <div className="App">
-      <h1>lets build excel beautifier</h1> 
+      <h1>lets build excel beautifier</h1>
+      <Home /> 
     </div>
   );
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+import { render } from "react-dom"
+
+const Home = () => {
+    return (
+        <>
+            <h1>Home</h1>
+        </>
+    )
+}
+
+
+export default Home
